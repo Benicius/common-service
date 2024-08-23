5be953ca4fb291e2cdf29db49fbb9429a087bf73
Benicius <beniciusclaro@hotmail.com>
2024-08-23 22:13:04 +0100

    adding consumer for the routing service

--- src/main/java/com/bdsystems/airport/common_service/controllers/TransportOrderController.java
+++ src/main/java/com/bdsystems/airport/common_service/controllers/TransportOrderController.java
@@ -3,9 +3,10 @@ package com.bdsystems.airport.common_service.controllers;
 import com.bdsystems.airport.common_service.domains.TransportOrder;
 import com.bdsystems.airport.common_service.domains.TransportOrderVO;
 import com.bdsystems.airport.common_service.services.TransportOrderService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/transport-order")
